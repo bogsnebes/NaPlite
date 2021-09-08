27ae7563b0bb216028834836a43005a86ee20510
bogsnebes <bogdan.bogotov123123@gmail.com>
2021-09-08 23:46:38 +0300

    created layouts

--- app/src/main/java/com/example/myapplication/ui/Ingredients/Ingredients.kt
+++ app/src/main/java/com/example/myapplication/ui/Ingredients/Ingredients.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
-class Ingredients : Fragment(R.layout.fragment_ingredients) {
+class Ingredients : Fragment(R.layout.fragment_viewpager_ingredients_recipe) {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
